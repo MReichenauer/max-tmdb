4e0401e40965c34ae45555fa479508d3a77bbda1
Max Reichenauer <max.reichenauer98@gmail.com>
2024-06-19 21:23:46 +0200

    Made the navbar position fixed and so that it inherits color and background color with a high z-index so it always is upfront, also made a toggle and close function for when the nav is in mobile mode. Reson for this is so if for example a user press on 'action' the nav will close after that click

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -7,8 +7,13 @@ import { ThemeContext } from "./ThemeProvider";
 import "../assets/scss/ThemeButton.scss";
 import { NavLink } from "react-router-dom";
 import useAllGenres from "../hooks/useAllGenres";
+import "../assets/scss/NavBar.scss";
 
 const NavBar = () => {
+  const [expanded, setExpanded] = useState(false);
+  const theme = useContext(ThemeContext);
+  const darkOrLight = theme.theme === "dark" ? "dark" : "light";
+
   const {
     data: allGenres,
     isError: isErrorGenres,
@@ -16,18 +21,25 @@ const NavBar = () => {
     isSuccess: isSuccessGenres,
   } = useAllGenres();
 
-  const theme = useContext(ThemeContext);
-  const darkOrLight = theme.theme === "dark" ? "dark" : "light";
-
   if (isErrorGenres)
     return <p>Ett fel uppstod vid hämtning av genres: {errorGenres.message}</p>;
+
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleClose = () => {
+    setExpanded(false);
+  };
 
   return (
     <>
       {isSuccessGenres && (
         <Navbar
+          className="fullNav"
           expand="lg"
           data-bs-theme={theme.theme === "dark" ? "dark" : "light"}
+          expanded={expanded}
         >
           <Container>
             <Navbar.Brand as={NavLink} to="/">
@@ -38,11 +50,14 @@ const NavBar = () => {
                 className={`themeButton ${darkOrLight}`}
                 onClick={theme.toggleTheme}
               />
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Toggle
+                aria-controls="basic-navbar-nav"
+                onClick={handleToggle}
+              />
             </div>
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link as={NavLink} to="/">
+                <Nav.Link as={NavLink} to="/" onClick={handleClose}>
                   Hem
                 </Nav.Link>
                 <NavDropdown title="Genre">
@@ -51,6 +66,7 @@ const NavBar = () => {
                       as={NavLink}
                       key={genre.id}
                       to={`/genres/${genre.id}`}
+                      onClick={handleClose}
                     >
                       {genre.name}
                     </NavDropdown.Item>
